e7cf9b8f6a098b74a5925a51f690470bc1989894
Chris Smith <chrisronsmith@gmail.com>
2021-09-18 23:23:19 -0400

    Use render prop in counterContext provider

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -6,11 +6,13 @@ export function Counter() {
   const [count, dispatch] = useCounter();
 
   return (
-    <counterContext.Provider value={count}>
-      <CounterDisplay />
-      <button onClick={() => dispatch("increment")}>+</button>
-      <button onClick={() => dispatch("decrement")}>-</button>
-      <button onClick={() => dispatch("reset")}>reset</button>
-    </counterContext.Provider>
+    <counterContext.Provider value={count} children={
+      <>
+        <CounterDisplay />
+        <button onClick={() => dispatch("increment")}>+</button>
+        <button onClick={() => dispatch("decrement")}>-</button>
+        <button onClick={() => dispatch("reset")}>reset</button>
+      </>
+    }/>
   );
 }
